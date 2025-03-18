f5ecd3428bc67a670f1984fc4ad86b1cbbf9be07
guilhermemartins07 <guigogomartins@gmail.com>
2025-03-18 16:58:02 -0300

    testes java script

---
+++ aula5/script.js
@@ -0,0 +1,22 @@
+//window.alert("hello word!")
+console.log("hello world!")
+
+let nome = prompt("Qual o seu nome?");
+console.log(nome);
+
+let i = 0
+while(i>10){
+    console.log(i)
+    //i++;
+    //i =i
+    //i +=1;
+}
+
+for(let i= 0; i < 10; i ++) {
+    console.log(i);
+}
+console.log('Exercício 1')
+for(let i = 1; i < 100; i +=2){
+    console.log(i);
+}
+
